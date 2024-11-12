b8d578e54f029a7c16798adf61386dc2b8192b2c
ezequiel.rodrigues <ezequiel.rodrigues@dbserver.com.br>
2024-11-12 20:00:33 -0300

    Modify: Formatação e resolução Exercício 4 e 5

--- Exercicio 3/Exercicio3.js
+++ Exercicio 3/Exercicio3.js
@@ -4,25 +4,26 @@ aleatoriamente e comparar com o que escolhemos. */
 
 // Resolução:
 
-function Jokenpo(escolhaJogador){
-    const opcoes = ["pedra","papel","tesoura"]
+function Jokenpo(escolhaJogador) {
+    const opcoes = ["pedra", "papel", "tesoura"]
 
-    const escolhaDaMaquina = opcoes[Math.floor(Math.random()*opcoes.length)]
+    const escolhaDaMaquina = opcoes[Math.floor(Math.random() * opcoes.length)]
 
     console.log(`Você escolheu: ${escolhaJogador}`)
     console.log(`A máquina escolheu: ${escolhaDaMaquina}`)
 
-    if(escolhaJogador === escolhaDaMaquina){
+    if (escolhaJogador === escolhaDaMaquina) {
         return "Empate!"
-    } else if(
+    } else if (
         (escolhaJogador === "pedra" && escolhaDaMaquina === "tesoura") ||
         (escolhaJogador === "papel" && escolhaDaMaquina === "pedra") ||
         (escolhaJogador === "tesoura" && escolhaDaMaquina === "papel")
-    ){
+    ) {
         return "Você ganhou!"
-    }else{
+    } else {
         return "Você perdeu..."
     }
 }
+
 // Teste do Jokenpo
 console.log(Jokenpo("tesoura"))
